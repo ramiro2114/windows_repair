3c9a983e4c7dfe1c237d254e2281502a566e2e70
Ramiro Alejandro Barberi <ramirobarberi@gmail.com>
2020-07-20 21:33:31 -0300

    The main file (always run as administrator).

---
+++ windows_repair.bat
@@ -0,0 +1,6 @@
+sfc /scannow
+DISM /Online /Cleanup-image /ScanHealth
+DISM /Online /Cleanup-Image /CheckHealth
+DISM /Online /Cleanup-image /RestoreHealth
+DISM /Online /Cleanup-Image /StartComponentCleanup
+PAUSE
